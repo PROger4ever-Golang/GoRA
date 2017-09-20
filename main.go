84b84d05d3d6b207b2d2f1193dae236b65c1008d
PROger4ever <a.pegushev@onetworent.com>
2017-09-20 14:36:53 +0500

    demonize GoRA a little bit

--- main.go
+++ main.go
@@ -2,20 +2,86 @@ package main
 
 import (
     "net/http"
-    "./routes"
     "flag"
     "strconv"
     "fmt"
+    "log"
+    "github.com/sevlyar/go-daemon"
+    "./routes"
+    "os"
 )
 
 var host = flag.String("h", "", "host/ip to bind")
 var port = flag.Int("p", 53837, "port to bind")
 
+//func main() {
+//    flag.Parse()
+//    bind := *host + ":" + strconv.Itoa(*port)
+//
+//    http.HandleFunc("/", routes.Router)
+//    fmt.Printf("Listening on %v...\n", bind)
+//
+//    //syscall.Umask(0777)
+//    //_, err := syscall.Setsid()
+//    //if err != nil {
+//    //    log.Fatalf("Error occured while Setsid: %v\n", err)
+//    //}
+//    //
+//    //os.Stdin.Close()
+//    //os.Stdout.Close()
+//    //os.Stderr.Close()
+//
+//    err := http.ListenAndServe(bind, nil)
+//    if err != nil {
+//        //TODO: Output to log file
+//        log.Fatalf("Error occured while ListenAndServe: %v\n", err)
+//    }
+//}
+
 func main() {
+    cntxt := &daemon.Context{
+        PidFileName: "pid",
+        PidFilePerm: 0644,
+        LogFileName: "log",
+        LogFilePerm: 0640,
+        WorkDir:     "./",
+        Umask:       027,
+        Args:        os.Args,
+    }
+
+    d, err := cntxt.Reborn()
+    if err != nil {
+        log.Fatal("Unable to run: ", err)
+    }
+    if d != nil {
+        return
+    }
+    defer cntxt.Release()
+
+    log.Print("- - - - - - - - - - - - - - -")
+    log.Print("daemon started")
+
+
+
     flag.Parse()
     bind := *host + ":" + strconv.Itoa(*port)
 
     http.HandleFunc("/", routes.Router)
-    fmt.Printf("Listening on %v...", bind)
-    http.ListenAndServe(bind, nil)
+    fmt.Printf("Listening on %v...\n", bind)
+
+    //syscall.Umask(0777)
+    //_, err := syscall.Setsid()
+    //if err != nil {
+    //    log.Fatalf("Error occured while Setsid: %v\n", err)
+    //}
+    //
+    //os.Stdin.Close()
+    //os.Stdout.Close()
+    //os.Stderr.Close()
+
+    err = http.ListenAndServe(bind, nil)
+    if err != nil {
+        //TODO: Output to log file
+        log.Fatalf("Error occured while ListenAndServe: %v\n", err)
+    }
 }
